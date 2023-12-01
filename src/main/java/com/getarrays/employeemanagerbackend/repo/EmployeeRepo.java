84bfc0a394780e1d27d56a4b269cb59f78924b33
Rushdha <rushdha2021@gmail.com>
2023-12-02 04:14:24 +0530

    added service class and handled an exception

--- src/main/java/com/getarrays/employeemanagerbackend/repo/EmployeeRepo.java
+++ src/main/java/com/getarrays/employeemanagerbackend/repo/EmployeeRepo.java
@@ -3,5 +3,10 @@ package com.getarrays.employeemanagerbackend.repo;
 import com.getarrays.employeemanagerbackend.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface EmployeeRepo extends JpaRepository<Employee, Long> {
+    void deleteEmployeeById(Long id);
+
+    Optional<Employee> findEmployeeById(Long id);
 }
